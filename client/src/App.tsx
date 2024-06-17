a6f983a22209b58e20147e2af7f86605cc228934
VlaDick2008 <sochukushat@yandex.ru>
2024-06-17 15:30:17 +0300

    Накидал вёрстку

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -41,17 +41,35 @@ function App() {
   }, []);
 
   return (
-    <main>
-      {modbusData?.arrayData.map((data, index) => {
-        if (!isConnected) return <div key={index}>Соединение потеряно...</div>;
+    <main className="flex items-start gap-5 p-10 min-h-screen bg-stone-900 text-stone-900">
+      <div className="border-2 border-slate-200 rounded bg-slate-100 p-2">
+        <h1 className="text-3xl">Первое устройство</h1>
+        <div className="w-full h-[2px] bg-stone-900 my-2" />
+        {modbusData?.arrayData.map((data, index) => {
+          if (!isConnected) return <div key={index}>Соединение потеряно...</div>;
 
-        return <div key={index}>{data}</div>;
-      })}
-      {abas?.arrayData.map((data, index) => {
-        if (!isConnected) return <div key={index}>Соединение потеряно...</div>;
+          return (
+            <div className="flex gap-2 text-xl" key={index}>
+              <p>Тег {index + 1}: </p>
+              <p>{data}</p>
+            </div>
+          );
+        })}
+      </div>
+      <div className="border-2 border-slate-200 rounded bg-slate-100 p-2">
+        <h1 className="text-3xl">Второе устройство</h1>
+        <div className="w-full h-[2px] bg-stone-900 my-2" />
+        {abas?.arrayData.map((data, index) => {
+          if (!isConnected) return <div key={index}>Соединение потеряно...</div>;
 
-        return <div key={index}>{data}</div>;
-      })}
+          return (
+            <div className="flex gap-2 text-xl" key={index}>
+              <p>Тег {index + 1}: </p>
+              <p>{data}</p>
+            </div>
+          );
+        })}
+      </div>
     </main>
   );
 }
